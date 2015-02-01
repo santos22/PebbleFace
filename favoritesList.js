33ff862c7b16d422df15bb6d08cf8d2bec6db3f2
santos22 <santosjavier22@gmail.com>
2015-02-01 10:01:47 -0600

    Watchapp
    Basic watchapp entirely in JS that displays your favorite phone contacts

---
+++ favoritesList.js
@@ -0,0 +1,73 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var Vector2 = require('vector2');
+
+var parseFeed = function(data, quantity) {
+  var items = [];
+  for(var i = 0; i < quantity; i++) {
+    // Always upper case the description string
+    var title = data.list[i].contacts[0].name;
+
+    // Get date/time substring
+    var time = data.list[i].number;
+    
+    // testing
+    console.log(title + ' YES!!!');
+    console.log(time + ' YES!!!!');
+
+    // Add to menu items array
+    items.push({
+      title:title,
+      subtitle:time
+    });
+  }
+
+  // Finally return whole array
+  return items;
+};
+
+// Show splash screen while waiting for data
+var splashWindow = new UI.Window();
+
+// Text element to inform user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text:'Loading contacts...',
+  font:'GOTHIC_28_BOLD',
+  color:'black',
+  textOverflow:'wrap',
+  textAlign:'center',
+	backgroundColor:'white'
+});
+
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
+
+// Make request to openweathermap.org
+ajax(
+  {
+    url:'https://raw.githubusercontent.com/santos22/PebbleFace/master/Favorites.json',
+    type:'json'
+  },
+  function(data) {
+    // Create an array of Menu items
+    var menuItems = parseFeed(data, 9);
+
+    // Construct Menu to show to user
+    var resultsMenu = new UI.Menu({
+      sections: [{
+        title: 'Favorites',
+        items: menuItems
+      }]
+    });
+
+    // Show the Menu, hide the splash
+    resultsMenu.show();
+    splashWindow.hide();
+  },
+  function(error) {
+    console.log('Download failed: ' + error);
+  }
+);
